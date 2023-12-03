ef1b897c55296dc1d5f15e8167815e4b7960dae5
Caio Borghi <caiohoborghi@gmail.com>
2023-12-03 18:15:00 -0300

    create and start rds postgres database

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     aws = {
@@ -14,7 +15,7 @@ variable "aws_region" {
 
 variable "ubuntu-ami" {
   type    = string
-  default = "ami-0b6c2d49148000cd5" #Ubuntu 22.04
+  default = "ami-0b6c2d49148000cd5" # Ubuntu 22.04
 }
 
 provider "aws" {
@@ -36,6 +37,17 @@ resource "aws_subnet" "load_test_subnet" {
   availability_zone = "sa-east-1a"
   tags = {
     Name = "load-test-subnet"
+  }
+}
+
+# Additional subnet (in a different AZ, e.g., sa-east-1b)
+resource "aws_subnet" "load_test_subnet_2" {
+  vpc_id     = aws_vpc.load_test_vpc.id
+  cidr_block = "10.0.2.0/24"  # Ensure a different CIDR block than the first subnet
+  map_public_ip_on_launch = true
+  availability_zone = "sa-east-1b"  # Different AZ
+  tags = {
+    Name = "load-test-subnet-2"
   }
 }
 
@@ -107,7 +119,73 @@ resource "aws_instance" "load_test_api" {
   user_data = file("./ec2-startup.sh")
 }
 
+resource "aws_db_instance" "load_test_postgres" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "15.4"
+  instance_class       = "db.t3.micro"
+  db_name              = "load_test_db" 
+  username             = "tester"
+  password             = "StressTestPassword"
+  parameter_group_name = "default.postgres15"
+  db_subnet_group_name = aws_db_subnet_group.load_test_subnet_group.name
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+
+  vpc_security_group_ids = [aws_security_group.load_test_db_security_group.id]
+
+  tags = {
+    Name = "load-test-postgres"
+  }
+}
+
+resource "aws_db_subnet_group" "load_test_subnet_group" {
+  name       = "load-test-subnet-group"
+  subnet_ids = [aws_subnet.load_test_subnet.id, aws_subnet.load_test_subnet_2.id]
+
+  tags = {
+    Name = "load-test-db-subnet-group"
+  }
+}
+
+resource "aws_security_group" "load_test_db_security_group" {
+  name        = "load-test-db-security-group"
+  description = "Allow PostgreSQL traffic"
+  vpc_id      = aws_vpc.load_test_vpc.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "load-test-db-security-group"
+  }
+}
+
+resource "null_resource" "db_setup" {
+  depends_on = [aws_db_instance.load_test_postgres, aws_security_group.load_test_db_security_group]
+
+  provisioner "local-exec" {
+    command = "sleep 60 && psql -h ${aws_db_instance.load_test_postgres.address} -U ${aws_db_instance.load_test_postgres.username} -d ${aws_db_instance.load_test_postgres.db_name} -f ./create-db.sql"
+    environment = {
+      PGPASSWORD = aws_db_instance.load_test_postgres.password
+    }
+  }
+}
+
 output "ec2_public_ip" {
   value = aws_instance.load_test_api.public_ip
   description = "The public IP address of the EC2 server instance."
 }
+
